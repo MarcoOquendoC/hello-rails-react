2dacde44d3401538e22c898af721281830cb7716
Marco Oquendo <marcooquendoc@gmail.com>
2023-03-01 16:18:08 -0500

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 Message.destroy_all
 
 Message.create! ([
-  {msg: 'Hello'},
-  {msg: 'Good Morning'},
-  {msg: 'Good Afternoon'},
-  {msg: 'Good Night'},
-  {msg: 'Greetings'}
+  {msg: 'Howdy, partner!'},
+  {msg: 'Hey there, freshman!'},
+  {msg: 'I come in peace!'},
+  {msg: 'Ahoy, matey!'},
+  {msg: 'At least, we meet for the first time for the last time!'}
 ])
 
 puts "Created #{Message.count} messages"
