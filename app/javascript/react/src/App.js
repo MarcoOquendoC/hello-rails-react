e3336dc671bdee67f5cb9aa989a8bea12c86605c
Marco Oquendo <marcooquendoc@gmail.com>
2023-03-05 04:33:03 -0500

    Implement lazy loading to reduce warnings (unsuccesfull)

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'; 
+import React, { lazy, Suspense } from 'react'; 
 import { Route, Routes } from 'react-router-dom';
-import Message from './components/Message.js';
+const Message = lazy(() => import('./components/Message.js'));
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Message />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Message />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
